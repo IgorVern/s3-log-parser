953b4ffa5532e30bf078d8da596dc324fcabe097
Igor Vernichenko <igor_vernichenko@epam.com>
2017-11-25 19:14:29 +0300

    wip

--- index.js
+++ index.js
@@ -1,64 +1,83 @@
-const readline = require('readline');
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB();
+const s3 = new AWS.S3();
+const readline = require('readline');
+const Rx = require('rxjs');
+const uuid = require('uuid');
+
 const parseLine = require('./logParser');
 
-const insertItem = (Item) => new Promise((resolve, reject) => {
-  dynamodb.putItem({
-    TableName: 's3WebLogs',
-    Item,
-  }, function(err, data) {
-    if (err) {
-      reject({
-        error: err,
-        message: 'Insert item error',
-        item: Item
-      });
-    } else {
-      resolve(data);
-    }
-  });
+const config = {
+	dynamoDB: {
+		tableName: 's3WebLogs'
+	}
+};
+
+const logError = (err) => console.error(JSON.stringify(err));
+
+const getLines = (params) => Rx.Observable.create((observer) => {
+	const fileStream = s3.getObject(params).createReadStream();
+
+	fileStream.on('error', (err) => {
+		logError({
+			error: err,
+			params,
+			message: 'Get object error'
+		});
+	});
+
+	const rl = readline.createInterface({
+		input: fileStream
+	});
+
+
+	rl.on('line', (line) => {
+		observer.next(line);
+	});
 });
 
-const processLog = (params) => new Promise((resolve, reject) => {
-  const fileStream = s3.getObject(params).createReadStream();
-  fileStream.on('error', err => reject({
-    error: err,
-    params,
-    message: 'Get object error'
-  }));
+const getDynamoItem = (logObject) => {
+	const item = {
+		_id: {
+			S: uuid.v4()
+		}
+	};
 
-  const rl = readline.createInterface({
-    input: fileStream
-  });
 
-  rl.on('line', (line) => {
-    const item = {
-      _id: {
-        N: `${Date.now()}`
-      }
-    };
-    const logObject = parseLine(line);
+	Object.keys(logObject).forEach(key => {
+		if (!logObject[key]) {
+			item[key] = {NULL: true};
+		} else {
+			item[key] = {S: logObject[key]};
+		}
+	});
 
-    Object.keys(logObject).forEach(key => {
-      if (!logObject[key]) {
-        item[key] = { NULL: true };
-      } else {
-        item[key] = { S: logObject[key] };
-      }
-    });
+	return item;
+};
 
-    insertItem(item).then(resolve).catch(reject);
-  });
+const prepareData = (Item) => ({
+	TableName: config.dynamoDB.tableName,
+	Item,
 });
+
+const insertItem = Rx.Observable.bindNodeCallback(dynamodb.putItem);
 
 exports.handler = (event, context, callback) => {
-  const promises = event.Records.map(record => processLog({
-      Bucket: record.s3.bucket.name,
-      Key: record.s3.object.key
-    }).catch(err => console.error(JSON.stringify(err)))
-  );
-
-  Promise.all(promises).then(() => callback());
+	Rx.Observable.from(event.Records)
+		.map((record) => ({
+			Bucket: record.s3.bucket.name,
+			Key: record.s3.object.key
+		}))
+		.map(getLines)
+		.mergeAll()
+		.map(parseLine)
+		.map(getDynamoItem)
+		.map(prepareData)
+		.map(insertItem)
+		.mergeAll()
+		.subscribe({
+			next: data => console.log('Item is inserted'),
+			error: err => callback(err),
+			complete: item => callback(),
+		});
 };
